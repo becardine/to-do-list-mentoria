8720c1abf384df39dd591b5bee663b5da6bf7f6a
becardine <brenda_alves@live.com>
2022-02-13 23:32:47 -0300

    config update

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async function (req, res) {
   try {
     const tasksList = await Task.find();
-    return res.render("index", {tasksList});
+    return res.render("index", { tasksList, task: null });
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -24,7 +24,29 @@ const createTasks = async function (req, res) {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    const tasksList = await Task.find();
+    res.render("index", { task, tasksList });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
+const updateOneTask = async (req, res) => {
+  try {
+    const task = req.body;
+    await Task.updateOne({ _id: req.params.id }, task);
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+
 module.exports = {
   getAllTasks,
   createTasks,
+  getById,
+  updateOneTask,
 };
